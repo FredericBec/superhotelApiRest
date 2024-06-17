ba0b66d18da956707e9141be61ae4f5c67757586
Frederic Bec <bec.frederic90@outlook.fr>
2024-06-17 15:26:42 +0200

    First commit

---
+++ src/main/java/fr/fms/superhotel/SuperhotelApplication.java
@@ -0,0 +1,13 @@
+package fr.fms.superhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperhotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperhotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/fms/superhotel/SuperhotelApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fms.superhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperhotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
