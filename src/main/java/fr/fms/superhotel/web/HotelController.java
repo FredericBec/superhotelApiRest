bf6abcfa9756191e600bf3328535f80baebf8b08
Frederic Bec <bec.frederic90@outlook.fr>
2024-06-18 15:05:18 +0200

    Add photo download and upload

--- src/main/java/fr/fms/superhotel/web/HotelController.java
+++ src/main/java/fr/fms/superhotel/web/HotelController.java
@@ -6,11 +6,16 @@ import fr.fms.superhotel.exception.RecordNotFoundException;
 import fr.fms.superhotel.mapper.HotelMapper;
 import fr.fms.superhotel.service.ImplHotelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -38,17 +43,19 @@ public class HotelController {
     @PostMapping("/hotels")
     public ResponseEntity<Hotel> saveHotel(@RequestBody HotelDto hotelDto){
         Hotel hotel = implHotelService.saveHotel(hotelMapper.mapToEntity(hotelDto));
-        if(Objects.isNull(hotel)) return ResponseEntity.noContent().build();
+        if(Objects.isNull(hotel)) {
+            return ResponseEntity.noContent().build();
+        }
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
-                .buildAndExpand()
+                .buildAndExpand(hotel.getId())
                 .toUri();
 
         return ResponseEntity.created(location).body(hotel);
     }
 
-    @PostMapping("/update/{id}")
+    @PostMapping("/hotels/{id}")
     public ResponseEntity<Hotel> updateHotel(@PathVariable("id") Long id, @RequestBody HotelDto hotelDto){
         Optional<Hotel> optional = implHotelService.readHotel(id);
         if(optional.isPresent()){
@@ -61,7 +68,7 @@ public class HotelController {
             hotel.setPrice(hotelDto.getPrice());
             hotel.setPhoto(hotelDto.getPhoto());
             hotel.setCity(hotelDto.getCity());
-
+            implHotelService.saveHotel(hotel);
             return ResponseEntity.ok(hotel);
         }
 
@@ -78,5 +85,32 @@ public class HotelController {
     public Hotel getHotelById(@PathVariable("id") Long id){
         return implHotelService.readHotel(id)
                 .orElseThrow(() -> new RecordNotFoundException("Id de l'hôtel " + id + " n'existe pas"));
+    }
+
+    @GetMapping(path = "/photo/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<?> getPhoto(@PathVariable("id") Long id) throws IOException{
+        byte[] file = null;
+        try{
+            Hotel hotel = implHotelService.readHotel(id).get();
+            if(hotel.getPhoto() == null) hotel.setPhoto("unknown.png");
+            file = Files.readAllBytes(Paths.get(System.getProperty("user.home") + "/Pictures/hotels/" + hotel.getPhoto()));
+        }catch (Exception e){
+            //log.error("problème avec download de l'image correspondant à l'hotel d'id : {}", id);
+            return ResponseEntity.internalServerError().body(e.getCause());
+        }
+        return ResponseEntity.ok().body(file);
+    }
+
+    @PostMapping(path = "/photo/{id}")
+    public ResponseEntity<?> uploadPhoto(MultipartFile file, @PathVariable Long id) throws Exception{
+        try {
+            Hotel hotel = implHotelService.readHotel(id).get();
+            hotel.setPhoto(file.getOriginalFilename());
+            Files.write(Paths.get(System.getProperty("user.home") + "/Pictures/hotels/" + hotel.getPhoto()), file.getBytes());
+            implHotelService.saveHotel(hotel);
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().body(e.getCause());
+        }
+        return ResponseEntity.ok().build();
     }
 }
