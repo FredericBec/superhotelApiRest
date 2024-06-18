b3104960e7531c3d95a5715db799632d83b869de
Frederic Bec <bec.frederic90@outlook.fr>
2024-06-18 11:42:58 +0200

    Add city and hotel repositories

---
+++ src/main/java/fr/fms/superhotel/dao/HotelRepository.java
@@ -0,0 +1,12 @@
+package fr.fms.superhotel.dao;
+
+import fr.fms.superhotel.entities.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface HotelRepository extends JpaRepository<Hotel, Long> {
+
+    List<Hotel> findAllByOrderByCityName();
+    List<Hotel> findByCityId(Long id);
+}
